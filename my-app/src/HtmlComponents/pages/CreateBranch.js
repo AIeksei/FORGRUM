532620b5c3cb4f766581a62605d67a784419eb2e
SquiryAP <artemper2002@gmail.com>
2022-05-26 01:26:36 +0400

    добавление поста

--- my-app/src/HtmlComponents/pages/CreateBranch.js
+++ my-app/src/HtmlComponents/pages/CreateBranch.js
@@ -1,20 +1,56 @@
 import '../Css/CreateBranch.css';
 import React from 'react';
+import { UseAuth } from '../Hook/UseAuth';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 const CreateBranch = ()=> {
-
+   const navigate = useNavigate();
+   const {id} = UseAuth();
+   const checker = true;
+   const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const title = form.title.value;
+    const text = form.text.value;
+    axios.post("http://localhost:8080/posts/", {
+    'title': title,
+    'text': text,
+    'postOwnerID': id
+ }, 
+ {
+	 headers: {
+		 Authorization: 'Basic dXNlcjpwYXNz' 
+   }
+}).then( function(res){
+    const postId = res.data.id;
+    axios.post("http://localhost:8080/tags/", {
+    'tag': "title",
+    'postID': postId,
+ }, 
+ {
+	 headers: {
+		 Authorization: 'Basic dXNlcjpwYXNz' 
+   }
+})
+}).catch(function(e){
+  alert(e)
+  checker = false;
+}).then( function(){
+  if (checker)
+  navigate("/branch/1", {replace: true})});
+ }
   return (
     <div className='bodyCreateBranch'>
-    <div className='borderCreateBranch'>
+    <form className='borderCreateBranch' onSubmit={handleSubmit}>
         <p className='CreateBranch'>Создание ветки</p>
         <input placeholder='Введите заголовок'
-            type='text' className='CreateBranchinput'></input>
+            type='text' className='CreateBranchinput' name = 'title'></input>
         <textarea placeholder='Текст ветки'
-            type='text' className='CreateBranchArea'></textarea>
+            type='text' className='CreateBranchArea' name = 'text'></textarea>
               <textarea placeholder='Теги'
             type='text' className='CreateBranchTag'></textarea>
-            
-        <input  type = 'button' value = "Создать ветку" className = 'CreateBranchbutton' ></input>
-    </div>
+           <button type="submit"  value = "Создать ветку" className = 'CreateBranchbutton' > Создать ветку </button>
+    </form>
 </div>
   );
 } 
